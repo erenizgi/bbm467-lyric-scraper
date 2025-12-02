5b66525c0ba726f2f40c3a28780c9006ab449e44
Eren <erenizgi001@gmail.com>
2025-12-02 15:18:31 +0300

    filtered  turkish and fixed balkans code

---
+++ fastText-language-separation/separateBalkans.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.read_csv("songs_with_language.csv")
+
+balkan_map = {
+    "Bulgarian": ["bg"],
+    "Greek": ["el"],
+    "Croatian": ["hr"],
+    "Hungarian": ["hu"],
+    "Romanian": ["ro"],
+    "Bosnian": ["bs"],
+    "Macedonian": ["mk"],
+    "Montenegrin": ["sr"],  # sr / sh genelde Montenegrin’i temsil ediyor
+    "Albanian": ["sq"],
+    "Serbian": ["sr"]
+}
+balkan_codes = [code for codes in balkan_map.values() for code in codes]
+
+
+balkan_songs = df[df["lang_fasttext"].isin(balkan_codes)]
+
+balkan_songs.to_csv("songs_balkan_only.csv", index=False)
+
+print(f"Ayıklandı! {len(balkan_songs)} Balkan kayıt bulundu.")
--- fastText-language-separation/separateTurkish.py
+++ fastText-language-separation/separateTurkish.py
@@ -7,8 +7,8 @@ df = pd.read_csv("songs_with_language.csv")
 # Eğer kolon adı 'language' ise:
 
 
-filtered = df[df["lang_fasttext"] == "bg" | df["lang_fasttext"] == "el" | df["lang_fasttext"] == "hr" | df["lang_fasttext"] == "hu" | df["lang_fasttext"] == "ro" | df["lang_fasttext"] == "bs" | df["lang_fasttext"] == "mk" | df["lang_fasttext"] == "cn" | df["lang_fasttext"] == "sq" | df["lang_fasttext"] == "sr"]
+filtered = df[df["lang_fasttext"] == "tr"]
 
-filtered.to_csv("song_balkans_only.csv", index=False)
+filtered.to_csv("songs_turkish_only.csv", index=False)
 
 print(f"Ayıklandı! {len(filtered)} Türkçe kayıt bulundu.")
